50836a68ec451582eb163161dd218a81464e1a08
Lucas Vargas <lucas.vargas.pina25@gmail.com>
2026-06-22 10:15:41 -0300

    feature: add a instancia das configuração banco/api

--- Executavel/main.py
+++ Executavel/main.py
@@ -6,3 +6,12 @@ from services.whatsapp_service import enviar_mensagem
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
+
+def main():
+
+    # As configurações
+    supabase_url = os.getenv("SUPABASE_URL")
+    supabase_key = os.getenv("SUPABASE_KEY")
+    zapi_url = f"https://api.z-api.io/instances/{os.getenv('ZAPI_INSTANCE_ID')}/token/{os.getenv('ZAPI_INSTANCE_TOKEN')}/send-text"
+
+    

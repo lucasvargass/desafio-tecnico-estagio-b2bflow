5206254427e522821f00b1693b997f072943c9ad
Lucas Vargas <lucas.vargas.pina25@gmail.com>
2026-06-22 09:46:07 -0300

    feature: add função de selecionar a table no banco com exceções

--- repositories/contato_repository.py
+++ repositories/contato_repository.py
@@ -6,3 +6,14 @@ def buscar_contatos(supabase_url, supabase_key, limite=3):
     if not supabase_url or not supabase_key:
         logging.error("Credenciais do SupaBase não configuradas.")
         return []
+    try:
+        supabase: Client = create_client(supabase_url, supabase_key)
+        response = supabase.table("contatos").select("nome_completo, numero").limit(limite).execute()
+        
+        logging.info(f"{len(response.data)} contatos encontrados.")
+        return response.data
+    
+    except Exception as e:
+        logging.error(f"Erro no banco: {e}")
+        return []
+        
